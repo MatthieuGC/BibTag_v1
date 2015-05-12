6017a38affab69a49ca29d0eabec690eb5238719
Matthieu Gourvénec <matthieu.gourvenec@gmail.com>
2015-05-12 16:30:18 +0200

    Generer les surrogates (.srg)

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -11,6 +11,13 @@ class ImportsController < ApplicationController
     @import = Import.new(import_params)
     if @import.save
       redirect_to imports_path, notice: "The BibTeX file #{@import.attachment.to_s.split('/').last} has been imported."
+      @bib = BibTeX.open("./public/#{@import.attachment.to_s}")
+      i = 0
+      while @bib[i] != nil do
+        @entry = @bib[i].to_s
+        File.open("./public/#{i}.srg", 'w+') { |f| f.write(@entry) }
+        i = i+1
+      end     
     else
       render "new"
     end
