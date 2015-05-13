96e3829a08477eecc52a6de66e98c6574fc8da87
Matthieu Gourvénec <matthieu.gourvenec@gmail.com>
2015-05-13 11:49:35 +0200

    Suppression des fichiers temporaires .bib après génération des surrogates .srg

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -9,15 +9,18 @@ class ImportsController < ApplicationController
 
   def create
     @import = Import.new(import_params)
+    @fName = @import.attachment.to_s.split('/').last.split('.').first
+    @uniqID = ('a'..'z').to_a.shuffle[0..7].join
     if @import.save
-      redirect_to imports_path, notice: "The BibTeX file #{@import.attachment.to_s.split('/').last} has been imported."
+      redirect_to imports_path, notice: "The BibTeX file #{@fName}.bib has been imported."
       @bib = BibTeX.open("./public/#{@import.attachment.to_s}")
       i = 0
       while @bib[i] != nil do
         @entry = @bib[i].to_s
-        File.open("./public/#{i}.srg", 'w+') { |f| f.write(@entry) }
+        File.open("./public/uploads/import/attachment/surrogates/#{@fName}-entry#{i}.srg", 'w+') { |f| f.write(@entry) }
         i = i+1
-      end     
+      end
+      FileUtils.rm_r("./public/uploads/import/attachment/tmp/#{@import.attachment.to_s.split('/')[-2]}")
     else
       render "new"
     end
@@ -26,7 +29,7 @@ class ImportsController < ApplicationController
   def destroy
     @import = Import.find(params[:id])
     @import.destroy
-    redirect_to imports_path, notice: "The BibTeX file #{@import.attachment.to_s.split('/').last} has been removed."
+    redirect_to imports_path, notice: "The BibTeX file #{@fName}.bib has been removed."
   end
 
   private def import_params
